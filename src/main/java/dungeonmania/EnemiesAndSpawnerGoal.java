6c1ee37550059dd9e658da3c10f033a4cec0f06e
Joshua Kasing Lee <joshualeee2018@gmail.com>
2021-10-25 20:38:18 +1100

    Implemented EnemiesAndSpawnerGoal Goal

--- src/main/java/dungeonmania/EnemiesAndSpawnerGoal.java
+++ src/main/java/dungeonmania/EnemiesAndSpawnerGoal.java
@@ -1,22 +1,27 @@
 package dungeonmania;
 
 public class EnemiesAndSpawnerGoal implements GoalComponent, Observer  {
-    public EnemiesAndSpawnerGoal() {
+    private int numLiving;
 
+    public EnemiesAndSpawnerGoal() {
+        numLiving = 0;
     }
 
     public boolean IsComplete() {
-        return false;
+        return (numLiving == 0);
     }
 
-    public void update(Subject obj) {
 
+    @Override
+    public boolean tryToAttach(Subject entity) {
+        if (entity instanceof Spider || entity instanceof Mercenary || entity instanceof ZombieToast || entity instanceof ZombieToastSpawner) {
+            entity.attach(this);
+            numLiving++;
+        }
     }
 
     @Override
-    public boolean tryToAttach(Entity entity) {
-        if (entity instanceof Spider || entity instanceof Mercenary || entity instanceof ZombieToast || entity instanceof ZombieToastSpawner) {
-            entity.attach(this);
-        }
-    }
+    public void update(Subject entity) {
+        numLiving--;
+    } 
 }
