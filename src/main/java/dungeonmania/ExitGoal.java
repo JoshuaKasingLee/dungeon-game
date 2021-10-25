74e92b5bd3f5039e7f9c3005acb7a92576ed4d14
Joshua Kasing Lee <joshualeee2018@gmail.com>
2021-10-25 20:39:04 +1100

    Implemented ExitGoal

--- src/main/java/dungeonmania/ExitGoal.java
+++ src/main/java/dungeonmania/ExitGoal.java
@@ -2,22 +2,29 @@ package dungeonmania;
 
 public class ExitGoal implements GoalComponent, Observer  {
 
-
+    private boolean onExit; 
     public ExitGoal() {
-
+        onExit = false;
     }
 
     public boolean isComplete() {
-        return true;
+        return onExit;
     }
 
     @Override
-    public boolean tryToAttach(Entity entity) {
+    public boolean tryToAttach(Subject entity) {
         if (entity instanceof Exit) {
             entity.attach(this);
         }
     }
-
-    public void update(Subject obj) {
+ 
+    @Override
+    public void update(Subject entity) {
+        Exit currExit = (Exit) entity;
+        if (currExit.hasPlayer()) {
+            onExit = true;
+        } else {
+            onExit = false;
+        }
     }
 }
