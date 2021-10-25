fd5cbb8f5bfa321e0dc92d140ffcbc12d6b505fc
Sami <z5207838@ad.unsw.edu.au>
2021-10-25 19:05:13 +1100

    Implemented tryToAttach function

--- src/main/java/dungeonmania/EnemiesAndSpawnerGoal.java
+++ src/main/java/dungeonmania/EnemiesAndSpawnerGoal.java
@@ -1,11 +1,22 @@
 package dungeonmania;
 
-public class EnemiesAndSpawnerGoal {
+public class EnemiesAndSpawnerGoal implements GoalComponent, Observer  {
     public EnemiesAndSpawnerGoal() {
 
     }
 
     public boolean IsComplete() {
         return false;
+    }
+
+    public void update(Subject obj) {
+
+    }
+
+    @Override
+    public boolean tryToAttach(Entity entity) {
+        if (entity instanceof Spider || entity instanceof Mercenary || entity instanceof ZombieToast || entity instanceof ZombieToastSpawner) {
+            entity.attach(this);
+        }
     }
 }
