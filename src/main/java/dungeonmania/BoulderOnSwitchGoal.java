ff072ce459e6ba7cbe01474d410f8ae8f98f1d10
Joshua Kasing Lee <joshualeee2018@gmail.com>
2021-10-25 20:36:57 +1100

    Implemented all functions to completed BoulderOnSwitchGoal

--- src/main/java/dungeonmania/BoulderOnSwitchGoal.java
+++ src/main/java/dungeonmania/BoulderOnSwitchGoal.java
@@ -1,22 +1,32 @@
 package dungeonmania;
 
-public class BoulderOnSwitchGoal implements GoalComponent, Observer  {
-    public BoulderOnSwitchGoal() {
+import java.util.ArrayList;
+import java.util.List;
 
+public class BoulderOnSwitchGoal implements GoalComponent, Observer  {
+    private int unpressedSwitches;
+    public BoulderOnSwitchGoal() {
+        unpressedSwitches = 0;
     }
 
     public boolean IsComplete() {
-        return false;
+        return (unpressedSwitches == 0);
     }
 
-    public void update(Subject obj) {
-
+    public void update(Subject entity) {
+        Switch currSwitch = (Switch) entity;
+        if (currSwitch.hasBoulder()) {
+            unpressedSwitches--;
+        } else {
+            unpressedSwitches++;
+        }
     }
 
     @Override
     public boolean tryToAttach(Entity entity) {
-        if (entity instanceof Boulder) {
+        if (entity instanceof Switch) {
             entity.attach(this);
+            unpressedSwitches++;
         }
     }
 }
