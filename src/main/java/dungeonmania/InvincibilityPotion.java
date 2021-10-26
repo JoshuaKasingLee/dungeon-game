11f16baf8f78ef0dcfa78b7e665d9eaf70cf2ca0
Kelly Zhou <kelly.zhou@student.unsw.edu.au>
2021-10-26 23:46:19 +1100

    Invicibility/InvisibilityPotion.java implementation added

--- src/main/java/dungeonmania/InvincibilityPotion.java
+++ src/main/java/dungeonmania/InvincibilityPotion.java
@@ -4,4 +4,10 @@ public class InvincibilityPotion extends Item {
     public InvincibilityPotion (String id) {
         super(id, "InvincibilityPotion");
     }
+
+    @Override
+    public void activate(Character character) {
+        super.activate(character);
+        character.setCharacterState(new InvincibleState(character));
+    }
 }
--- src/main/java/dungeonmania/InvisibilityPotion.java
+++ src/main/java/dungeonmania/InvisibilityPotion.java
@@ -5,5 +5,11 @@ public class InvisibilityPotion extends Item {
     public InvisibilityPotion (String id) {
         super(id, "InvisibilityPotion");
     }
+
+    @Override
+    public void activate(Character character) {
+        super.activate(character);
+        character.setCharacterState(new InvisibleState(character));
+    }
     
 }
