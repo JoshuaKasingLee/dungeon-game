a0d6d5bc59442d4f12334494e9dc13aa06f08bf1
Kelly Zhou <kelly.zhou@student.unsw.edu.au>
2021-10-27 00:04:47 +1100

    Weapons/Protection Items.java added default uses left

--- src/main/java/dungeonmania/Item.java
+++ src/main/java/dungeonmania/Item.java
@@ -1,15 +1,25 @@
 package dungeonmania;
 
+
+// assume that:
+// items that need to be triggered to use;
+// potions, bombs
+// items that are auto-used:
+// weaponry, armour/shields, one-ring
+
 public abstract class Item {
     private String id;
     private String type;
+    private int usesLeft;
 
     public Item (String id, String type) {
         this.id = id;
         this.type = type;
+        this.usesLeft = 1;
     }
 
     public void activate(Character character) {
+        this.usesLeft = usesLeft - 1;
     
     }
 
@@ -27,6 +37,20 @@ public abstract class Item {
      */
     public String getType() {
         return type;
+    }
+
+    /**
+     * @return int return the usesLeft
+     */
+    public int getUsesLeft() {
+        return usesLeft;
+    }
+
+    /**
+     * @param usesLeft the usesLeft to set
+     */
+    public void setUsesLeft(int usesLeft) {
+        this.usesLeft = usesLeft;
     }
 
 }
