b365c47db76f9e00db43a835d360062fb0016a8a
Kelly Zhou <kelly.zhou@student.unsw.edu.au>
2021-10-27 17:38:37 +1100

    OneRing.java added up implementation and tidied code

--- src/main/java/dungeonmania/HealthPotion.java
+++ src/main/java/dungeonmania/HealthPotion.java
@@ -1,7 +1,18 @@
 package dungeonmania;
 
+
 public class HealthPotion extends Item {
+
     public HealthPotion (String id) {
         super(id, "HealthPotion");
+    }
+
+    /**
+     * regenerates given entity's health back to full
+     */
+    @Override
+    public void activate(Character character) {
+        super.activate(character);
+        character.setHealth(Character.ORIGINAL_HEALTH);
     }
 }
--- src/main/java/dungeonmania/OneRing.java
+++ src/main/java/dungeonmania/OneRing.java
@@ -4,4 +4,14 @@ public class OneRing extends Item {
     public OneRing (String id) {
         super(id, "OneRing");
     }
+
+    /**
+     * regenerates character's health back to full
+     * auto-used when character dies if exists in inventory
+     */
+    @Override
+    public void activate(Character character) {
+        super.activate(character);
+        character.setHealth(Character.ORIGINAL_HEALTH);
+    }
 }
