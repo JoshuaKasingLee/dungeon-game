274ec59a7a5b375bfae18521768a147588bda2bd
Sami <z5207838@ad.unsw.edu.au>
2021-10-27 23:33:49 +1100

    Added constructor for CompositeGoal

--- src/main/java/dungeonmania/CompositeGoal.java
+++ src/main/java/dungeonmania/CompositeGoal.java
@@ -1,9 +1,14 @@
 package dungeonmania;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public abstract class CompositeGoal implements GoalComponent, Observer {
     private List<GoalComponent> subgoals;
+
+    public CompositeGoal() {
+        this.subgoals = new ArrayList<GoalComponent>();
+    }
 
     public void addSubgoal(GoalComponent subgoal) {
         subgoals.add(subgoal);
