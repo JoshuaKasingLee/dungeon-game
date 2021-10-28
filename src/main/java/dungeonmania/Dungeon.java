48507a61526d2d6e2a45dda2634ff5934ab96ef7
Sami <z5207838@ad.unsw.edu.au>
2021-10-28 16:48:52 +1100

    Added multiple methods to Dungeon to work with entities at certain position

--- src/main/java/dungeonmania/Dungeon.java
+++ src/main/java/dungeonmania/Dungeon.java
@@ -2,14 +2,20 @@ package dungeonmania;
 
 import java.util.List;
 
+import dungeonmania.util.Position;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class Dungeon {
 
     private String dungeonName;
     private Gamemode gameMode;
     private String dungeonId;
     private List<Entity> entities;
-    private List<Items> inventory;    // subject to change name or class to entity
-    private List<Items> buildables; // subject to change name or class to entity
+    private List<Entity> inventory;    // subject to change name or class to entity
+    private List<Entity> buildables; // subject to change name or class to entity
     private List<GoalComponent> simpleGoals;
     private GoalComponent overallGoal;
 
@@ -39,6 +45,44 @@ public class Dungeon {
         simpleGoals.add(simpleGoal);
     }
     
+    public List<Entity> getEntities(Position position) {
+        List<Entity> entitiesAtPos = new ArrayList<>();
+        for (Entity entity : entities) {
+            if (entity.getPosition().equals(position)) {
+                entitiesAtPos.add(entity);
+            }
+        }
+        return entitiesAtPos;
+    }
+
+    public Door getDoor(Position position) {
+        List<Entity> entityList = getEntities(position);
+        for (Entity entity : entityList) {
+            if (entity instanceof Door) {
+                return (Door) entity;
+            }
+        }
+         return null;
+    }
+
+    public void addTo(Entity entity, Position position) {
+        entity.setPosition(position);
+        addEntity(entity);
+    }
+
+    public void removeFrom(Entity entity) {
+        entities.remove(entity);
+    }
+
+    public Player getPlayer() {
+        for (Entity entity : entities) {
+            if (entity instanceof Player) {
+                return entity;
+            }
+        }
+        return null;
+    }
+        
 
     /**
      * @return String return the dungeonName
@@ -55,16 +99,16 @@ public class Dungeon {
     }
 
     /**
-     * @return String return the gameMode
+     * @return Gamemode return the gameMode
      */
-    public String getGameMode() {
+    public Gamemode getGameMode() {
         return gameMode;
     }
 
     /**
      * @param gameMode the gameMode to set
      */
-    public void setGameMode(String gameMode) {
+    public void setGameMode(Gamemode gameMode) {
         this.gameMode = gameMode;
     }
 
@@ -82,7 +126,6 @@ public class Dungeon {
         this.dungeonId = dungeonId;
     }
 
-
     /**
      * @return List<Entity> return the entities
      */
@@ -96,9 +139,6 @@ public class Dungeon {
     public void setEntities(List<Entity> entities) {
         this.entities = entities;
     }
-
-
-
 
     /**
      * @return List<GoalComponent> return the simpleGoals
@@ -126,6 +166,13 @@ public class Dungeon {
      */
     public void setOverallGoal(GoalComponent overallGoal) {
         this.overallGoal = overallGoal;
+    }
+
+    /**
+     * @return List<Entity> return the inventory
+     */
+    public List<Entity> getInventory() {
+        return inventory;
     }
 
 }
