f672bdc4a8c08ec16ce293b576f3497f0341c4fd
Kelly Zhou <kelly.zhou@student.unsw.edu.au>
2021-10-28 19:26:42 +1100

    Bomb.java completed bomb implementation

--- src/main/java/dungeonmania/Bomb.java
+++ src/main/java/dungeonmania/Bomb.java
@@ -1,8 +1,11 @@
 package dungeonmania;
 
+import java.util.ArrayList;
+import java.util.List;
 import dungeonmania.util.Position;
 
 public class Bomb extends Item {
+    public final static int BLAST_RADIUS = 3;
 
     public Bomb (Position position, String id, Dungeon dungeon) {
         super(position, id, "Bomb", dungeon);
@@ -14,21 +17,51 @@ public class Bomb extends Item {
     @Override
     public void activate(Character character) {
         super.activate(character);
-        // Position currPos = character.getPosition();
-        
+        // setPosition(character.getPosition());
+        getDungeon().addTo(this, character.getPosition());
     }
 
     /**
      * creations explosion - destroys all entities in the bomb's blast radius, except for the character
+     * assume bomb does not explode if boulder is already on switch?
      */
     public void explode() {
+        Position bombPos = getPosition();
+        for (Position p : blastRadiusPositions(bombPos)) {
+            getDungeon().removeFrom(p);
+        }
+        // remove bomb
+        getDungeon().removeFrom(this);
     }
 
-    // /**
-    //  * @param position the position to set
-    //  */
-    // public void setPosition(Position position) {
-    //     this.position = position;
-    // }
+    private static List<Position> blastRadiusPositions(Position pos) {
+        List<Position> blastRadiusPositions = new ArrayList<Position>();
+        int currX = pos.getX();
+        int currY = pos.getY();
+        for (int i = currX - BLAST_RADIUS; i <= currX + BLAST_RADIUS; i++) {
+            for (int j = currY - BLAST_RADIUS; j <= currY + BLAST_RADIUS; j++) {
+                Position p = new Position(i, j);
+                if (calculateDistance(p, pos) <= BLAST_RADIUS && !p.equals(pos)) {
+                    // System.out.println(calculateDistance(p, pos));
+                    blastRadiusPositions.add(p);
+                }
+            }
+        }
+        return blastRadiusPositions;
+    }
+
+    private static int calculateDistance(Position pos1, Position pos2) {
+        Position dirVector = Position.calculatePositionBetween(pos1, pos2);
+        int squaredDist = (dirVector.getX() * dirVector.getX()) + (dirVector.getY() * dirVector.getY());
+        // always rounds up
+        return (int) Math.ceil(Math.sqrt(squaredDist));
+    }
+
+    public static void main(String[] args) {   
+        List<Position> blastRadiusPositions = blastRadiusPositions(new Position(0,0));
+        for (Position p : blastRadiusPositions) {
+            System.out.println(p.toString());
+        }
+    }
 
 }
