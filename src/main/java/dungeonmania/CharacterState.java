6bb639ffbc38b04b959f5795ca858e2ad6da5f0d
Sami <z5207838@ad.unsw.edu.au>
2021-10-30 00:56:41 +1100

    Syntax fixes

--- src/main/java/dungeonmania/CharacterState.java
+++ src/main/java/dungeonmania/CharacterState.java
@@ -2,6 +2,7 @@ package dungeonmania;
 
 public interface CharacterState {
     
+    
     public abstract void battleEnemy(Enemy enemy);
     public abstract void updateState();
     public abstract String getType();
