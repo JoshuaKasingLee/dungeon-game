03f5f0014e82e1c86fd42007d910524c8968a0fc
Cyrus <cyrusyschow@gmail.com>
2021-10-29 11:13:53 +1100

    Implementing updateMove functions into enemy entities

--- src/main/java/dungeonmania/Mercenary.java
+++ src/main/java/dungeonmania/Mercenary.java
@@ -1,10 +1,24 @@
 package dungeonmania;
 
+import java.util.List;
+import java.util.ArrayList;
+import java.util.Collections;
+
 import dungeonmania.util.Position;
 
 public class Mercenary extends Enemy {
     public static final int ORIGINAL_HEALTH = 10;
     public static final int MERCENARY_ATTACK_DAMAGE = 4;
+
+    private int x = getPosition().getX();
+    private int y = getPosition().getY();
+    // distance list
+    // position list
+    private List<Position> possiblePositions = getPossiblePositions();
+    private List<Double> distanceOfPositions = getDistanceOfPositions();
+
+    private Position playerPosition = getDungeon().getPlayer().getPosition();
+
 
     public Mercenary(Position position, String id, Dungeon dungeon) {
         super(position, id, "Mercenary", dungeon);
@@ -14,7 +28,51 @@ public class Mercenary extends Enemy {
         setIsInteractable(true);
     }
 
-    public void updatePosition() {   
+    @Override
+    public void updatePosition() {
+        // create an array of 4 possible positions between character and mercenary (up, down, left, right)
+        // go in the shortest direction between the two // if blocked, don't move
+        int direction = distanceOfPositions.indexOf(Collections.min(distanceOfPositions));
+
+        switch(direction) {
+            case 0:
+                moveUp();
+                break;
+            case 1:
+                moveDown();
+                break;
+            case 2:
+                moveLeft();
+                break;
+            case 3:
+                moveRight();
+                break;
+        }
+    }
+
+    public List<Position> getPossiblePositions() {
+        List<Position> possiblePositions = new ArrayList<>();
+        // Up position
+        possiblePositions.add(new Position(x, y-1));
+
+        // Down position
+        possiblePositions.add(new Position(x, y+1));
+
+        // Left position
+        possiblePositions.add(new Position(x-1, y));
+
+        // Right position
+        possiblePositions.add(new Position(x+1, y));
+        return possiblePositions;
     }
     
+    public List<Double> getDistanceOfPositions() {
+        List<Double> distanceOfPositions = new ArrayList<Double>();
+        for (Position possiblePosition : possiblePositions) {
+            Position tempPosition = Position.calculatePositionBetween(possiblePosition, playerPosition);
+            distanceOfPositions.add(Math.sqrt(tempPosition.getX()*tempPosition.getX() + 
+                                              tempPosition.getY()*tempPosition.getY()));
+        }
+        return distanceOfPositions;
+    }
 }
--- src/main/java/dungeonmania/Spider.java
+++ src/main/java/dungeonmania/Spider.java
@@ -1,19 +1,104 @@
 package dungeonmania;
 
+import java.util.List;
+import java.util.ArrayList;
+
 import dungeonmania.util.Position;
 import dungeonmania.util.Direction;
 
 public class Spider extends Enemy {
     public static final int ORIGINAL_HEALTH = 5;
     public static final int SPIDER_ATTACK_DAMAGE = 2;
+    private static final int CLOCKWISE = 1;
+    private static final int ANTICLOCKWISE = -1;
+
+    private int positionNumber;
+    // 1 is clockwise, -1 is anticlockwise
+    private int direction = CLOCKWISE;
+
+    // Positions
+    private Position startingPosition;
+    private ArrayList<Position> adjacentPositions;
 
     public Spider(Position position, String id, Dungeon dungeon) {
         super(position, id, "Spider", dungeon);
         this.setHealth(ORIGINAL_HEALTH);
         this.setAttackDamage(SPIDER_ATTACK_DAMAGE);
+        this.startingPosition = position;
+        this.adjacentPositions = setPositions(position);
+        this.positionNumber = 0;
         // need to consider edge case where spider movement goes off the map - what happens?
     }
 
+    public ArrayList<Position> setPositions(Position position) {
+        //GET ADJACENT POSITIONS
+        return new ArrayList<Position>(position.getAdjacentPositions());
+    }
+
+    public List<Position> getAdjacentPositions() {
+        return adjacentPositions;
+    }
+
+    @Override
     public void updatePosition() {
+        if (getPosition().equals(startingPosition)) {
+            if (checkForBoulder(adjacentPositions.get(1))) {
+                return;
+            } else {
+                setPosition(adjacentPositions.get(1));
+                setPositionNumber(1);
+            }
+        } else {
+            if (!checkForBoulder(adjacentPositions.get(getNextPositionNumber()))) {
+                setPosition(adjacentPositions.get(getNextPositionNumber()));
+                setPositionNumber(getNextPositionNumber());
+            } else {
+                direction *= -1;
+            }
+        }
+    }
+
+    public void setPositionNumber(int number) {
+        this.positionNumber = number;
+    }
+
+    public int getPositionNumber() {
+        if (this.positionNumber < 0) {
+            return (this.positionNumber % 8 + 8);
+        }
+        return (this.positionNumber % 8);
+    }
+
+    public int getNextPositionNumber() {
+        if ((this.positionNumber + direction) < 0) {
+            return ((this.positionNumber + direction) % 8 + 8);
+        }
+        return ((this.positionNumber + direction) % 8);
+    }
+
+    public boolean checkForBoulder(Position position) {
+        for (Entity entity : getDungeon().getEntities(position)) {
+            if (entity instanceof Boulder) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public static void main(String[] args) {
+        Dungeon dungeon = new Dungeon("Dungeon", "Standard", "1");
+        Spider thisSpider = new Spider(new Position(2, 2), "Kelly", dungeon);
+        System.out.println(thisSpider.getAdjacentPositions());
+        // Boulder newBoulder = new Boulder(new Position(0, -1), "test", dungeon);
+        System.out.println(thisSpider.getPosition());
+        thisSpider.updatePosition();
+        System.out.println(thisSpider.getPosition());
+        thisSpider.updatePosition();
+        System.out.println(thisSpider.getPosition());
+        thisSpider.updatePosition();
+        System.out.println(thisSpider.getPosition());
+        thisSpider.updatePosition();
+        System.out.println(thisSpider.getPosition());
+        thisSpider.updatePosition();
     }
 }
