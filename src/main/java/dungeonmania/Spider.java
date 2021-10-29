5edc7d5e29c84f1377f820cdece131b754dbbb29
Sami <z5207838@ad.unsw.edu.au>
2021-10-29 19:25:31 +1100

    Syntax fixes

--- src/main/java/dungeonmania/Spider.java
+++ src/main/java/dungeonmania/Spider.java
@@ -4,7 +4,6 @@ import java.util.List;
 import java.util.ArrayList;
 
 import dungeonmania.util.Position;
-import dungeonmania.util.Direction;
 
 public class Spider extends Enemy {
     public static final int ORIGINAL_HEALTH = 5;
