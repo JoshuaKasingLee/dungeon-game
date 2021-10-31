f1cbc26bdabd456ecaf44883e468f50e3658194d
Joshua Kasing Lee <joshualeee2018@gmail.com>
2021-11-01 04:00:28 +1100

    tag

--- src/main/java/dungeonmania/DungeonManiaController.java
+++ src/main/java/dungeonmania/DungeonManiaController.java
@@ -808,7 +808,7 @@ public class DungeonManiaController {
 
     
     /** 
-     * create a list of entity responses
+     * create list of entity responses
      * @return List<EntityResponse>
      */
     public List<EntityResponse> createEntityResponseList() {
