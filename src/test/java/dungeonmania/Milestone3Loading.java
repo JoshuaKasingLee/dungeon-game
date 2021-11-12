25cc843ee191efc533153c088e7fb6f961c98ef9
Sami <z5207838@ad.unsw.edu.au>
2021-11-12 17:02:36 +1100

    Added multiple new tests to test crafting new items and using sceptre

--- src/test/java/dungeonmania/Milestone3Loading.java
+++ src/test/java/dungeonmania/Milestone3Loading.java
@@ -47,4 +47,113 @@ public class Milestone3Loading {
         assertEquals(true, dungeonInfo.getEntities().stream().anyMatch(x -> x.getType().equals("assassin")));
     }
     
+    @Test
+    public void testCraftSceptreWithWoodAndTreasure() {
+        DungeonManiaController controller = new DungeonManiaController();
+        assertDoesNotThrow(() -> controller.newGame("craftingSceptre", "Standard"));
+        assertDoesNotThrow(() -> controller.tick(null, Direction.DOWN));
+        assertDoesNotThrow(() -> controller.tick(null, Direction.DOWN));
+        DungeonResponse dungeonInfo = controller.tick(null, Direction.DOWN);
+        assertEquals(true, dungeonInfo.getInventory().stream().anyMatch(x -> x.getType().equals("wood")));
+        assertEquals(true, dungeonInfo.getInventory().stream().anyMatch(x -> x.getType().equals("treasure")));
+        assertEquals(true, dungeonInfo.getInventory().stream().anyMatch(x -> x.getType().equals("sun_stone")));
+
+        dungeonInfo = controller.build("sceptre");
+        assertEquals(false, dungeonInfo.getInventory().stream().anyMatch(x -> x.getType().equals("wood")));
+        assertEquals(false, dungeonInfo.getInventory().stream().anyMatch(x -> x.getType().equals("treasure")));
+        assertEquals(false, dungeonInfo.getInventory().stream().anyMatch(x -> x.getType().equals("sun_stone")));
+
+        assertEquals(true, dungeonInfo.getInventory().stream().anyMatch(x -> x.getType().equals("sceptre")));
+        assertDoesNotThrow(() -> controller.saveGame("craftingSceptre"));
+        dungeonInfo = controller.loadGame("craftingSceptre");
+        assertEquals(true, dungeonInfo.getInventory().stream().anyMatch(x -> x.getType().equals("sceptre")));
+    }
+
+    @Test
+    public void testCraftSceptreWithArrowAndKey() {
+        DungeonManiaController controller = new DungeonManiaController();
+        assertDoesNotThrow(() -> controller.newGame("craftingSceptre", "Standard"));
+        assertDoesNotThrow(() -> controller.tick(null, Direction.RIGHT));
+        assertDoesNotThrow(() -> controller.tick(null, Direction.RIGHT));
+        assertDoesNotThrow(() -> controller.tick(null, Direction.RIGHT));
+        DungeonResponse dungeonInfo = controller.tick(null, Direction.RIGHT);
+        assertEquals(true, dungeonInfo.getInventory().stream().anyMatch(x -> x.getType().equals("arrow")));
+        assertEquals(true, dungeonInfo.getInventory().stream().anyMatch(x -> x.getType().equals("key")));
+        assertEquals(true, dungeonInfo.getInventory().stream().anyMatch(x -> x.getType().equals("sun_stone")));
+
+        dungeonInfo = controller.build("sceptre");
+        assertEquals(false, dungeonInfo.getInventory().stream().anyMatch(x -> x.getType().equals("arrow")));
+        assertEquals(false, dungeonInfo.getInventory().stream().anyMatch(x -> x.getType().equals("key")));
+        assertEquals(false, dungeonInfo.getInventory().stream().anyMatch(x -> x.getType().equals("sun_stone")));
+
+        assertEquals(true, dungeonInfo.getInventory().stream().anyMatch(x -> x.getType().equals("sceptre")));
+        assertDoesNotThrow(() -> controller.saveGame("craftingSceptre"));
+        dungeonInfo = controller.loadGame("craftingSceptre");
+        assertEquals(true, dungeonInfo.getInventory().stream().anyMatch(x -> x.getType().equals("sceptre")));
+    }
+
+    @Test
+    public void testLoadingArmour() {
+        DungeonManiaController controller = new DungeonManiaController();
+        assertDoesNotThrow(() -> controller.loadGame("briber"));
+
+    }
+    @Test
+    public void testMindControlMercenary() {
+        DungeonManiaController controller = new DungeonManiaController();
+        DungeonResponse dungeonInfo = controller.loadGame("briber");
+
+        assertEquals(true, dungeonInfo.getEntities().stream().anyMatch(x -> x.getType().equals("mercenary")));
+        assertEquals(true, dungeonInfo.getInventory().stream().anyMatch(x -> x.getType().equals("sceptre")));
+
+        Dungeon activeGame = controller.getActiveGame();
+
+        Mercenary mercenary = (Mercenary)activeGame.getEntities().stream().filter(n -> n.getType().equals("mercenary")).findFirst().orElse(null);
+        assertEquals(false, mercenary.isAlly());
+
+        assertDoesNotThrow(() -> controller.tick("3", null));
+        assertEquals(true, mercenary.isAlly());
+        assertEquals("MercControlled", mercenary.getMercenaryState().getType());
+
+    }
+
+    @Test
+    public void testMindControlAssassin() {
+        DungeonManiaController controller = new DungeonManiaController();
+        DungeonResponse dungeonInfo = controller.loadGame("briber");
+
+        assertEquals(true, dungeonInfo.getEntities().stream().anyMatch(x -> x.getType().equals("assassin")));
+        assertEquals(true, dungeonInfo.getInventory().stream().anyMatch(x -> x.getType().equals("sceptre")));
+
+        Dungeon activeGame = controller.getActiveGame();
+
+        Assassin assassin = (Assassin)activeGame.getEntities().stream().filter(n -> n.getType().equals("assassin")).findFirst().orElse(null);
+        assertEquals(false, assassin.isAlly());
+
+        assertDoesNotThrow(() -> controller.tick("3", null));
+        assertEquals(true, assassin.isAlly());
+        assertEquals("MercControlled", assassin.getMercenaryState().getType());
+    }
+
+    @Test
+    public void testCraftMidnightArmour() {
+        DungeonManiaController controller = new DungeonManiaController();
+        DungeonResponse dungeonInfo = controller.loadGame("briber");
+
+        assertEquals(true, dungeonInfo.getInventory().stream().anyMatch(x -> x.getType().equals("armour")));
+        assertEquals(true, dungeonInfo.getInventory().stream().anyMatch(x -> x.getType().equals("sun_stone")));
+
+        dungeonInfo = controller.build("midnight_armour");
+
+        assertEquals(true, dungeonInfo.getInventory().stream().anyMatch(x -> x.getType().equals("midnight_armour")));
+
+        assertEquals(false, dungeonInfo.getInventory().stream().anyMatch(x -> x.getType().equals("armour")));
+        assertEquals(false, dungeonInfo.getInventory().stream().anyMatch(x -> x.getType().equals("sun_stone")));
+
+        assertDoesNotThrow(() -> controller.saveGame("craftingMidnightArmour"));
+        dungeonInfo = controller.loadGame("craftingMidnightArmour");
+        assertEquals(true, dungeonInfo.getInventory().stream().anyMatch(x -> x.getType().equals("midnight_armour")));
+    }
+
 }
+
