08baf0bd2e3acb2c2d7de81495c08b718b0a11d0
Kelly Zhou <kelly.zhou@student.unsw.edu.au>
2021-11-12 19:22:10 +1100

    Hydra.java removed redundant attributes and code

--- src/main/java/dungeonmania/Hydra.java
+++ src/main/java/dungeonmania/Hydra.java
@@ -2,17 +2,10 @@ package dungeonmania;
 
 import dungeonmania.util.Position;
 import java.util.Random;
-import java.util.List;
-import java.util.ArrayList;
-import java.util.Collections;
 
 public class Hydra extends Enemy {
     public static final int ORIGINAL_HEALTH = 15;
     public static final int HYDRA_ATTACK_DAMAGE = 4;
-    private int x = getXPosition();
-    private int y = getYPosition();
-    private List<Position> possiblePositions;
-    private List<Double> distanceOfPositions;
 
     public Hydra(Position position, Dungeon dungeon) {
         super(position, dungeon);
@@ -75,55 +68,10 @@ public class Hydra extends Enemy {
     }
 
     /** 
-     * returns list of positions zombie toast can move into
-     * @return List<Position>
-     */
-    public List<Position> getPossiblePositions() {
-        List<Position> possiblePositions = new ArrayList<Position>();
-        // Up position
-        possiblePositions.add(new Position(x, y-1));
-        // Down position
-        possiblePositions.add(new Position(x, y+1));
-        // Left position
-        possiblePositions.add(new Position(x-1, y));
-        // Right position
-        possiblePositions.add(new Position(x+1, y));
-        return possiblePositions;
-    }
-
-    /** 
-     * returns list of distance between player and possible zombie toast positions
-     * @return List<Double>
-     */
-    public List<Double> getDistanceOfPositions() {
-        List<Double> distanceOfPositions = new ArrayList<Double>();
-        // Loop through and add the distance of between two positions
-        for (Position possiblePosition : possiblePositions) {
-            distanceOfPositions.add(calculateDistance(possiblePosition, getDungeon().getPlayer().getPosition()));
-        }
-        return distanceOfPositions;
-    }
-
-    /** 
-     * returns distance between two positions, rounded to nearest whole number
-     * @param pos1
-     * @param pos2
-     * @return double
-     */
-    private static double calculateDistance(Position pos1, Position pos2) {
-        Position dirVector = Position.calculatePositionBetween(pos1, pos2);
-        double squaredDist = (dirVector.getX() * dirVector.getX()) + (dirVector.getY() * dirVector.getY());
-        // always rounds up
-        return (double) (Math.sqrt(squaredDist));
-    }
-
-    /** 
      * @return String
      */
     @Override
     public String setType() {
         return "hydra";
     }
-
-    
 }
