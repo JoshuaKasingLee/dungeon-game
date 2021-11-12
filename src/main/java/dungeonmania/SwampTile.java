8ec6f159633e19af9c5112f2b527003482ddc918
Cyrus <cyrusyschow@gmail.com>
2021-11-13 09:01:54 +1100

    Adding test file maps to the repository

--- src/main/java/dungeonmania/SwampTile.java
+++ src/main/java/dungeonmania/SwampTile.java
@@ -4,19 +4,13 @@ import dungeonmania.util.Direction;
 import dungeonmania.util.Position;
 
 public class SwampTile extends StaticEntity {
-    // Counter that counts the number of ticks that the player is on the square
-    // Update function increases the counter, and update function resets counter if does not have player
-    // Can boulders go through swamps?
-    // TODO: Check whether player is called first or static entity
-    // TODO: Add a movement_factor and remove it from Gamemode
-
     public SwampTile(Position position, Dungeon dungeon, double movementFactor) {
         super(position, dungeon);
         setMovementFactor(movementFactor);
     }
 
     /**
-     * TODO: Update Javadoc and Function
+     * Updates the static entity tile
      * @param direction
      */
     public void update(Direction direction) {
