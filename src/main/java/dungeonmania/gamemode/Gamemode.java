621ea2355b7767ca77f01a4e03e231ea218c8896
Joshua Kasing Lee <joshualeee2018@gmail.com>
2021-11-13 13:32:28 +1100

    removed unused setters in gamemode

--- src/main/java/dungeonmania/gamemode/Gamemode.java
+++ src/main/java/dungeonmania/gamemode/Gamemode.java
@@ -56,38 +56,10 @@ public abstract class Gamemode {
 
 
     /**
-     * @param battle the battle to set
-     */
-    public void setBattle(boolean battle) {
-        this.battle = battle;
-    }
-
-    /**
-     * @param spawnTimer the spawnTimer to set
-     */
-    public void setSpawnTimer(int spawnTimer) {
-        this.spawnTimer = spawnTimer;
-    }
-
-    /**
-     * @param startingHealth the startingHealth to set
-     */
-    public void setStartingHealth(int startingHealth) {
-        this.startingHealth = startingHealth;
-    }
-
-    /**
      * @return boolean return the invincible
      */
     public boolean isInvincible() {
         return invincible;
-    }
-
-    /**
-     * @param invincible the invincible to set
-     */
-    public void setInvincible(boolean invincible) {
-        this.invincible = invincible;
     }
 
     /**
@@ -97,11 +69,5 @@ public abstract class Gamemode {
         return hydraSpawnTimer;
     }
 
-    /**
-     * @param hydraSpawnTimer the hydraSpawnTimer to set
-     */
-    public void setHydraSpawnTimer(int hydraSpawnTimer) {
-        this.hydraSpawnTimer = hydraSpawnTimer;
-    }
 
 }
